7caefc96e83661c0a861093b73cb64582e74d401
[Karolina Kedzior] <[Karolina.Kedzior@fis.agh.edu.pl]>
2017-11-23 15:46:20 +0100

    AutoCorrector and basic Threading usage

--- kol1.py
+++ kol1.py
@@ -20,64 +20,93 @@ import sys
 import numpy as np
 import time
 
+import threading
+
+
 std_deviation =  0.1
 def gaussian_turbulation(orientation):
 	return np.random.normal(orientation, std_deviation)
 
-class FlightSimulator:
-	def __init__(self, start_orientation, step_correction, perfect_orientation, generate_turbulation = gaussian_turbulation):
-		self.current_orientation = start_orientation
-		self.step_correction =step_correction
-		self.perfect_orientation = perfect_orientation
-		self.turbulation_generator = generate_turbulation
-	
-	def correct_tilt(self):
-		self.current_orientation = self.current_orientation % 360 
-		if self.current_orientation < self.perfect_orientation:
-			self.current_orientation = self.current_orientation + self.step_correction
-		elif self.current_orientation > self.perfect_orientation:
-			self.current_orientation = self.current_orientation - self.step_correction
+class AutoCorrector:
+	def __init__(self, desired_value, step_correction, minimal_value, maximum_value):
+		self.desired_value = desired_value
+		self.step_correction = step_correction
+		self.value_min = minimal_value
+		self.value_max = maximum_value
+	def change_desired_value(self, new_value, isDegree = True):
+		if not isDegree:
+			self.desired_value = new_value * 180.0/Math.PI
+		else:
+			self.desired_value = new_value
+	def correct_value(self, value_in):
+		value_in = value_in % 360 
+		correct_value = value_in
+		if value_in < self.desired_value:
+			corrected_value = value_in + self.step_correction
+		elif value_in > self.desired_value:
+			corrected_value = value_in - self.step_correction
+		return correct_value
+				
+class FlightSimulator(threading.Thread):
+	def __init__(self, start_orientation = 90.0, orientation_corrector = None, generate_turbulation_fun = gaussian_turbulation, ):
+
+		super(FlightSimulator, self).__init__()
 		
+		self.current_orientation = start_orientation
+		self.orientation_corrector = orientation_corrector
+		self.turbulation_generator = generate_turbulation_fun
+		self._stop_event = True
 	def generate_turbulation(self):
 		self.current_orientation =  self.turbulation_generator(self.current_orientation) 
-	
+	def apply_correction(self):
+		if self.orientation_corrector:
+			self.current_orientation = self.orientation_corrector.correct_value(self.current_orientation)
 	def print_current_orientation(self,  unit = 'degrees'):
 		print('Current Orientation: ' + '{:.4f}'.format(self.current_orientation) + " " + unit)
+	def run(self):
+		self._stop_event = True
+		while True:
+			if self._stop_event:
+				flight_sim.generate_turbulation() 
+				flight_sim.apply_correction()
+				time.sleep(0.3)
+			else:
+				break
+			
+	def stop(self):
+		self._stop_event = False
 		
-		
+			
 if __name__ == "__main__":
 
-	flight_sim = FlightSimulator( 20.0 ,0.2, 90.0)
+	flight_sim = FlightSimulator( start_orientation =20.0 , orientation_corrector = AutoCorrector(90.0,0.2, 0.0, 360.0) )
 	
 	while True:
 		print('''Choose option:
-		[1] - Start flight simulation. (Hit Ctrl+C to return)
-		[2] - Show flight current orientation
-		[3] - Change flight correction parameters.
-		[4] - Exit ''' )
+		[1] - Start flight simulation.
+		[2] - Close program ''' )
 		option = raw_input().lower();
 		
 		if(option == '1'):
 			print('Simulation ON')
-			try:
-				while True:
-					flight_sim.generate_turbulation() 
-					flight_sim.correct_tilt()
-					flight_sim.print_current_orientation()
-					time.sleep(0.3)
-			except KeyboardInterrupt:
-				pass
-			print('\nSimulation OFF\n')
-		elif(option== '2'):
-			flight_sim.print_current_orientation()			
-		elif(option== '3'):
-			try:
-				flight_sim.step_correction = float(raw_input("Enter step correction value: "))
-				flight_sim.perfect_orientation = float(raw_input('Enter desired orientation: '))
-			except Exception:
-				pass
-			print('Parameters changed sucesssfuly!\n')
-		elif(option == '4'):
+			flight_sim.start()
+			while True:
+				print('''Choose option:
+[1] - Show flight current orientation (Ctr+C to return)
+[2] - Stop simulation ''' )
+				option = raw_input().lower();
+				if(option== '1'):
+					try:
+						while True:
+							flight_sim.print_current_orientation()	
+							time.sleep(0.3)
+					except KeyboardInterrupt:
+						pass
+				elif(option== '2'):
+					flight_sim.stop()
+					print('\nSimulation OFF\n')
+					break
+		elif(option == '2'):
 			break
 
 
